8cdf9c3250b2101dd422717605b74558e6ac771a
Kasper Jensen <kasper.jensen@solarisbank.de>
2020-02-23 12:15:07 +0100

    Add stupid transaction endpoint

--- src/crystal-blockchain.cr
+++ src/crystal-blockchain.cr
@@ -11,6 +11,15 @@ module Crystal::Blockchain
     blockchain.to_json
   end
 
+  post "/add_transaction" do |env|
+    sender = env.params.json["sender"].as(String)
+    amount = env.params.json["amount"].as(Int64)
+    recipient = env.params.json["recipient"].as(String)
+
+    data = "I, #{sender}, am sending #{amount} coins to #{recipient}"
+    data
+  end
+
   spawn do
     loop do
       block = Block.generate(blockchain.last, "I, Bob, am sending 5 coins to Alice")
