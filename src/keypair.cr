cd6b74d06685bf24c2d9f6983a20131b6deb1b6d
Kasper Jensen <kasper.jensen@solarisbank.de>
2020-03-07 21:47:10 +0100

    Generate key files

--- src/keypair.cr
+++ src/keypair.cr
@@ -4,7 +4,18 @@ class Keypair
   def self.generate(name)
     puts "Generating keys named #{name}"
 
-    private_key = OpenSSL::RSA.new(32)
-    puts private_key.to_pem
+    private_key = OpenSSL::RSA.new(512)
+    private_pem = private_key.to_pem
+    public_pem = private_key.public_key.to_pem
+
+    private_path = "#{name}_private.pem"
+    public_path = "#{name}_public.pem"
+
+    if File.exists?(public_path) || File.exists?(private_path)
+      puts "Keys already present, abandoning!"
+    end
+
+    File.write(private_path, private_pem)
+    File.write(public_path, public_pem)
   end
 end
